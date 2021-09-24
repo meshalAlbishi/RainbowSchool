e2ca1f0296b96280bce8a7d6116372f56d3eec4d
meshalAlbishi <m.albishi25@gmail.com>
2021-09-25 02:39:44 +0300

    Add files via upload

---
+++ RainbowSchool/Classes/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace RainbowSchool.Classes
+{
+    public class Teacher
+    {
+        public readonly int ID;
+        public string Name {get; set;}
+
+        public Teacher(int ID, string name)
+        {
+            this.ID = ID;
+            this.Name = name;
+        }
+
+        //Class and section
+    }
+}
---
+++ RainbowSchool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RainbowSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.ReadLine();
+        }
+    }
+}
